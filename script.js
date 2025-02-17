d5a1a651a3e0093eb197f615cd450cfd0c1f617c
kartoph <24b_pav@liceum.ztu.edu.ua>
2025-02-17 11:21:58 +0200

    ff
    ff

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log('Scrip connected!');
+
+document.getElementById('Wishes_btn').addEventListener('click' , () => (
+    //alert('Button clicked!')
+    document.getElementById('p_Wishes').innerText = 'Кохайте одне одного!'
+
+));
